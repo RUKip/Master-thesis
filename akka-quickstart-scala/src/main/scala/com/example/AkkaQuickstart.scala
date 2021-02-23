b6e5e00c8d7b6e4078f89ed42d72c35fe8e1b59e
r kip <gjr.kip@gmail.com>
2021-02-23 13:05:40 +0100

    small update in import

--- akka-quickstart-scala/src/main/scala/com/example/AkkaQuickstart.scala
+++ akka-quickstart-scala/src/main/scala/com/example/AkkaQuickstart.scala
@@ -1,27 +1,27 @@
 //#full-example
 package com.example
 
-import akka.actor.typed.javadsl.Behaviors
+import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorSystem, Behavior}
 import com.example.actors.Node
 import com.typesafe.config.ConfigFactory
 
 object AkkaQuickstart extends App {
-//  //#actor-system
-//  val greeterMain: ActorSystem[GreeterMain.SayHello] = ActorSystem(GreeterMain(), "AkkaQuickStart")
-//  //#actor-system
-//
-//  //#main-send-messages
-//  greeterMain ! SayHello("Charles")
-//  //#main-send-messages
 
+    //Startup cluster?
+
+    //This matches with the config file
+    val ports = Seq(25251, 25252, 0)
+    ports.foreach(startup)
+
+
+    //Startup agents
     val (tree, mapping): (TreeNode, Map[Int, TreeNode]) = TreeHelper.initATree()
     val system: ActorSystem[Node.PrintGraph] = ActorSystem(Node(tree, mapping), "AkkaQuickStart")
 
     system ! Node.PrintGraph()
 
-    val ports = Seq(25251, 25252, 0)
-    ports.foreach(startup)
+
 
     def startup(port: Int): Unit = {
         // Override the configuration of the port
