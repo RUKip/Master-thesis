867359aaae99d09772e4c35908444eaf51b96f4e
RUKip <gjr.kip@gmail.com>
2021-06-19 13:14:23 +0200

    updated tree generation

--- akka-quickstart-scala/src/main/scala/com/example/Generator.scala
+++ akka-quickstart-scala/src/main/scala/com/example/Generator.scala
@@ -1,5 +1,7 @@
 package com.example
 
+import scala.annotation.tailrec
+
 object Generator extends App {
 
   val threshold :Int = 3
@@ -9,7 +11,7 @@ object Generator extends App {
     createTree(id)
   })
 
-  def createTree(id: Int): Unit = {
+  def createTreeOld(id: Int): Unit = {
     println("Trying to generate...")
     val base = InitializationHelper.createTreeStructure(3, 4, 1, 1, 1, 10)
     if (base.size < threshold) {
@@ -20,6 +22,23 @@ object Generator extends App {
       InitializationHelper.storeTree(base, file_name)
       println("Finished")
       println("***************")
+    }
+  }
+
+  @tailrec
+  def createTree(id: Int): Unit = {
+    println("Trying to generate...")
+    val base = InitializationHelper.init(4, 7)
+    if (base.nonEmpty) {
+      if (base.size < threshold) {
+        createTree(id)
+      } else {
+        println("Created base: " + base.toMap)
+        val file_name = "generated_trees/" + id + "_generated_tree.json"
+        InitializationHelper.storeTree(base, file_name)
+        println("Finished")
+        println("***************")
+      }
     }
   }
 }
